f12043302d8e427df116b626f0bc106141657116
LoadingElii <delijhiabrown@gmail.comgit config --global user.nam delijhiabrown@gmail.comgit config --global user.email delijhiabrown@gmail.comgit config --global user.email delijhiabrown@gmail.com>
2025-11-09 23:01:20 -0600

    added redis for caching game and predictions and cache.py for cache methods

---
+++ app/core/cache.py
@@ -0,0 +1,15 @@
+import redis
+import json
+from app.core.config import settings
+
+r = redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0, decode_responses=True)
+
+
+def set_cache(key: str, value, expire_seconds: int | None = None):
+    r.set(key, json.dumps(value), ex=expire_seconds)
+
+def get_cache(key: str):
+    value = r.get(key)
+    if value:
+      return json.loads(value)
+    return None
--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,8 @@ class Settings(BaseSettings):
     app_name: str = "Game API"
     debug: bool = True
     version: str = "1.0.0"
+    redis_host: str = "localhost"
+    redis_port: int = 6379
 
     class Config:
         env_file = ".env"
